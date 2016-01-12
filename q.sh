eed30d5dbf1cf5d13e686cf0a53d1d2360ea8916
lizhengqiang <lizhengqiang@mouge.wang>
2016-01-12 16:26:45 +0800

    Quick Commit:functions

--- q.sh
+++ q.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-
-#get highest tag number
-VERSION=`git describe --abbrev=0 --tags`
-
-#replace . with space so can split into an array
-VERSION_BITS=(${VERSION//./ })
-
-#get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-VNUM3=$((VNUM3+1))
-
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
-echo "Commit $NEW_TAG"
-
+echo "Commit $1"
 git add -A
-git commit -m "Quick Commit:$NEW_TAG $1"
+git commit -m "Quick Commit:$1"
 
 git push
